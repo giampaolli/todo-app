8ee9e47ab56e75685a4a654c6937ba681f8665ef
giampaolli <felipegiampaoli@yahoo.com.br>
2017-07-18 16:30:16 -0300

    pageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props =>(
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Loren ipsum dolor sil emet...</p>
+        <h2>Missão e visão</h2>
+        <p>Loren ipsum dolor sil emet...</p>
+        <h2>Imprensa</h2>
+        <p>Loren ipsum dolor sil emet...</p>
     </div>
 )
