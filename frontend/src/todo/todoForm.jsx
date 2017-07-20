80c79a743402946b903b7dbfbe134467b8823c55
Felipe Giampaoli <felipeg@aquarius.cpqd.com.br>
2017-07-20 18:04:48 +0100

    ações dos botões

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -11,7 +11,9 @@ export default props => (
                 onChange={props.handleChange}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton  style='primary' icon='plus' onClick={props.handleAdd}> </IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}> </IconButton>
+            <IconButton style='info' icon='search' onClick={props.handleSearch}> </IconButton>
+            <IconButton style='defaul' icon='close' onClick={props.handleClear}></IconButton>
         </Grid>
     </div>
 )
