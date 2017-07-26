62b5b152e1917a6b840c428161fb2a409e4cb404
giampaolli <felipegiampaoli@yahoo.com.br>
2017-07-26 12:22:11 -0300

    redux dev tool

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import IconButton from '../template/iconButton'
 
-export default props => {
+const TodoList = props => {
 
     const renderRows = () => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                <td className={todo.done ? 'markAsDone' : ''
+                }>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                    onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                        onClick={() =>
+                            props.handleMarkAsDone(todo)}></IconButton>
                     <IconButton style='warning' icon='undo' hide={!todo.done}
-                    onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                        onClick={() =>
+                            props.handleMarkAsPending(todo)}></IconButton>
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
-                    onClick={() => props.handleRemove(todo)}></IconButton>
+                        onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
         ))
     }
+
     return (
         <table className='table'>
             <thead>
@@ -32,5 +37,7 @@ export default props => {
             </tbody>
         </table>
     )
-
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+export default connect(mapStateToProps)(TodoList)
